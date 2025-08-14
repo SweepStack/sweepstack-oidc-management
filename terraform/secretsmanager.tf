60e68fd4b6dcce23ea9573817fa6d1c55589b603
Trey <treyoung12@yahoo.com>
2025-08-13 22:57:29 -0400

    create secret

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -1,3 +1,8 @@
-resource "aws_secretsmanager_secret" "example" {
+resource "aws_secretsmanager_secret" "TF_API_KEY" {
   name = "TF_API_KEY"
+}
+
+resource "aws_secretsmanager_secret_version" "TF_API_KEY" {
+  secret_id     = aws_secretsmanager_secret.TF_API_KEY.id
+  secret_string = jsonencode("thisisasecret")
 }
