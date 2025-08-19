5d7feb751598e9deb243fd34cba47be926c6e6c4
Trey <treyoung12@yahoo.com>
2025-08-19 02:44:19 -0400

    add auth vars

--- hcp-aws/variables.tf
+++ hcp-aws/variables.tf
@@ -1,8 +1,18 @@
+variable "TFC_WORKSPACE_NAME" {
+  description = "The name of the current Terraform Cloud workspace."
+  type        = string
+}
+
+data "tfe_workspace" "current_workspace" {
+  name         = var.TFC_WORKSPACE_NAME
+  organization = local.org_name
+}
+
 resource "tfe_variable" "tfc_aws_provider_auth" {
   key          = "TFC_AWS_PROVIDER_AUTH"
   value        = "true"
   category     = "env"
-  workspace_id = terraform.workspace.id
+  workspace_id = data.tfe_workspace.current_workspace.id
 }
 
 resource "tfe_variable" "tfc_example_role_arn" {
@@ -10,5 +20,5 @@ resource "tfe_variable" "tfc_example_role_arn" {
   key          = "TFC_AWS_RUN_ROLE_ARN"
   value        = aws_iam_role.oidc_terraform_connector_iam.arn
   category     = "env"
-  workspace_id = terraform.workspace.id
+  workspace_id = data.tfe_workspace.current_workspace.id
 }
